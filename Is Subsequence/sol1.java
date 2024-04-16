f733f16a6da2252c17738038529079107ec9cb82
Prashant Shrivastava <98282430+prashant2602@users.noreply.github.com>
2024-04-16 13:29:43 +0530

    Create sol1.java

---
+++ Is Subsequence/sol1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int s_len=s.length();
+        int t_len=t.length();
+        int i=0;
+        int j=0;
+        while(i<s_len && j<t_len){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        if(i==s_len){
+            return true;
+        }
+        return false;
+    }
+}
