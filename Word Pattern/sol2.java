0415dd67a86f23de95c61e720cf2fc1391d3f1fa
Prashant Shrivastava <98282430+prashant2602@users.noreply.github.com>
2024-05-12 02:49:36 +0530

    Update sol2.java

--- Word Pattern/sol2.java
+++ Word Pattern/sol2.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean wordPattern(String s, String ss) {
-        HashMap<Character, String> a = new HashMap<>();
-        String[] details = ss.split(" ",);
+        HashMap<Character, String> map = new HashMap<>();
+        String[] details = ss.split(" ");
         if(s.length() != details.length){
             return false;
         }
             for (int i = 0; i < s.length(); i++) {
-                if(a.containsKey(s.charAt(i))){
-                    String n = a.get(s.charAt(i));
-                    if(!n.equals(split_string[i])){
+                if(map.containsKey(s.charAt(i))){
+                    String str = map.get(s.charAt(i));
+                    if(!str.equals(details[i])){
                         return false;
                     }
                 }
-                else if(a.containsValue(split_string[i])){
+                else if(map.containsValue(details[i])){
                     return false;
                 }
                 else{
-                    a.put(s.charAt(i),split_string[i]);
+                    map.put(s.charAt(i),details[i]);
                 }
             }
             
