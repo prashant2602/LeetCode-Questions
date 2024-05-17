7829ffc476aedf2884647db4a61a28ab001a969f
Prashant Shrivastava <98282430+prashant2602@users.noreply.github.com>
2024-05-18 04:39:25 +0530

    Create sol1.java

---
+++ Best Time to Buy and Sell Stock II/sol1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int start = prices[0];
+        int len = prices.length;
+        for(int i = 1;i<len; i++){
+            if(start < prices[i]) {
+                max = max+ prices[i] - start;
+            }
+            start = prices[i];
+        }
+        return max;
+    }
+}
