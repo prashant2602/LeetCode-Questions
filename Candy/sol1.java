1950638082ad4709ec6975d63428ee40e24ee2df
Prashant Shrivastava <98282430+prashant2602@users.noreply.github.com>
2024-05-28 16:38:25 +0530

    Create sol1.java

---
+++ Candy/sol1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int candyArr[] = new int[ratings.length];
+        for(int i=0;i<ratings.length;i++){
+            candyArr[i]=1;
+        }
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i-1]<ratings[i]){
+                candyArr[i]=candyArr[i-1]+1;
+            }
+        }
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                if(candyArr[i]<=candyArr[i+1]){
+                    candyArr[i]=candyArr[i+1]+1;
+                }
+            }
+            
+        }
+        int sum=0;
+        for(int i=0;i<ratings.length;i++){
+            sum=sum+candyArr[i];
+        }
+        return sum;
+    }
+}
